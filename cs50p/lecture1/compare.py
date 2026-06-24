9cc35221d0f0fb4ae1187616337c31247f45b7a5
Julien Steff <julienxaviersteff@gmail.com>
2026-06-23 19:08:59 -0500

    cs50p: lecture 1 — compare.py (if/elif/else)

---
+++ cs50p/lecture1/compare.py
@@ -0,0 +1,10 @@
+# CS50P L1 — basic comparison with if/elif/else, integer input
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
